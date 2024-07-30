c53993ab16f233f09b7fa8e3a43520e31d0394b7
DeepSoupp <158445678+DeepSoupp@users.noreply.github.com>
2024-07-30 09:11:46 +0900

    solve : [BOJ] N과 M （3） _240728

---
+++ 05/N과 M （3）.py
@@ -0,0 +1,15 @@
+n,m = list(map(int,input().split()))
+
+list = []
+
+def dfs():
+    if len(list)==m:
+        print(' '.join(map(str,list)))
+        return
+    
+    for i in range(1,n+1):
+            list.append(i)
+            dfs()
+            list.pop()
+            
+dfs()
